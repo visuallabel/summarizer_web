b59ddc9c431f3992bf673242ce7de446ccf35767
Petri Rantanen <petri.rantanen@tut.fi>
2015-10-30 13:48:03 +0200

    copyrights updated

--- src/otula/summarizerweb/Response.java
+++ src/otula/summarizerweb/Response.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 Tampere University of Technology, Pori Unit
+ * Copyright 2014 Tampere University of Technology, Pori Department
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
